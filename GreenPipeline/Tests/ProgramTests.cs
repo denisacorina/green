9c8412b362830eb78ed7ad20ef6f3947e3be2d95
Deni <denisa.obreja@yahoo.com>
2025-01-27 19:09:16 +0200

    first init

---
+++ GreenPipeline/Tests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace GreenPipeline.Tests;
+public class ProgramTests
+{
+    [Fact]
+    public void TestHttpClientConfiguration()
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(Array.Empty<string>());
+        var services = builder.Services;
+        
+
+        var serviceProvider = services.BuildServiceProvider();
+        var httpClient = serviceProvider.GetService(typeof(HttpClient)) as HttpClient;
+
+        Assert.NotNull(httpClient);
+        Assert.Equal("https://localhost:7183/", httpClient.BaseAddress.ToString());
+    }
+}
